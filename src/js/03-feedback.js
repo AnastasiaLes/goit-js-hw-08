e6a2e5126ec4a3ca412991e8880c2410cb64b75d
Anastasia Les <anastasia@sonarmedia.com>
2021-12-26 15:19:41 +0200

    form bugfix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,44 +2,51 @@ import lodash, { throttle } from 'lodash';
 
 const { stringify } = require("querystring");
 
-console.log('Hello world');
+
 const form = document.querySelector('.feedback-form');
-const message = document.querySelector('.feedback-form textarea');
-const email = document.querySelector('.feedback-form input');
-let feedbackFormData = {};
+const formMessage = document.querySelector('.feedback-form textarea');
+const formEmail = document.querySelector('.feedback-form input');
+const submitBtn = document.querySelector('.feedback-form button')
+let feedbackFormData = {
+    // email: formEmail.value,
+    // message: formMessage.value
+};
+
 
 const STORAGE_KEY = 'feedback-form-state';
 const obj = JSON.parse(localStorage.getItem(STORAGE_KEY));
-// console.dir(form);
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onTextareaInput, 500) );
 
-populateTextarea(feedbackFormData);
+populateTextarea(obj);
 
 function onFormSubmit(event) {
     event.preventDefault();
     event.target.reset();
     console.log(obj);
-    localStorage.removeItem(STORAGE_KEY);
-    
+    localStorage.removeItem(STORAGE_KEY);  
 }
 
 function onTextareaInput(event) {
+
     feedbackFormData[event.target.name] = event.target.value;
     // console.log(feedbackFormData);
-    
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+    if (feedbackFormData) {
+        feedbackFormData.email = formEmail.value,
+        feedbackFormData.message = formMessage.value  
+       }
+        
+    const stringifiedData = JSON.stringify(feedbackFormData)
+    localStorage.setItem(STORAGE_KEY, stringifiedData);
+   
 }
 
 function populateTextarea() {
-    let savedForm = {};
-
-    
     if (obj) {
-        message.value = obj.message;
-        email.value = obj.email;
-        // console.log(obj);
+        formEmail.value = obj.email;
+        formMessage.value = obj.message;
     };
+   
 }
 
